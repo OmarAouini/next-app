965329930a092eebac8e6acf28350d4a9103eb6b
OmarAouini <aouini.omar93@gmail.com>
2021-10-09 04:24:39 +0200

    wip navbar

--- components/topNavbar.js
+++ components/topNavbar.js
@@ -1,18 +1,17 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import Link from 'next/link'
-import Image from 'next/image'
 import { PersonCircle, PersonFill } from 'react-bootstrap-icons'
 
 const TopNavbar = () => {
     return (
         <Navbar bg="dark" variant="dark" className="p-4" sticky="top">
             <Container fluid className="navbar-inner-container text-white">
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Brand href="/"><b>Home</b></Navbar.Brand>
             <Nav className="me-auto d-inline-block">
-            <span className="mx-3"><Link as="/employees" href="/employees">Employees </Link></span>
+            <span className="mx-3"><Link as="/employees" href="/employees">Employees</Link></span>
             <span className="mx-3"><Link as="/projects" href="/projects">Projects </Link></span>
             <span className="mx-3"><Link as="/tasks" href="/tasks">Tasks</Link></span>
-            <span>
+            {true ? <span>
                 <Link as="/login" href="/login" passHref>
                     <a>
                     <PersonFill size={32}/>
@@ -20,6 +19,7 @@ const TopNavbar = () => {
                     </a>
                 </Link>
             </span>
+            :
             <span>
                 <Link as="/logout" href="/logout" passHref>
                     <a>
@@ -28,6 +28,7 @@ const TopNavbar = () => {
                     </a>
                 </Link>
             </span>
+            }
             </Nav>
             </Container>
         </Navbar>
--- pages/api/employees.js
+++ pages/api/employees.js
@@ -1,4 +1,6 @@
-//handler main
-export default function handler(req,res) {
 
+export default function handler(req,res) {
+    if (req.method !== 'POST') {
+        res.status(405).send("only POST allowed!")
+    }
 }
